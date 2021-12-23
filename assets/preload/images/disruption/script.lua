805434c1a970356bad835d360f69ee719100c0ae
ninjaninja140 <88348015+ninjaninja140@users.noreply.github.com>
2021-12-23 11:46:49 +0000

    all new stuff added

--- assets/preload/images/disruption/script.lua
+++
@@ -1,9 +0,0 @@
-
-
-function onUpdate(elapsed)
-    if curStep >= 0 then
-      songPos = getSongPosition()
-      local currentBeat = (songPos/1000)*(bpm/60)
-      doTweenY(bfTweenY, 'dylank', 300-10*math.sin((currentBeat*0.005)*math.pi),0.001)
-    end
-  end
